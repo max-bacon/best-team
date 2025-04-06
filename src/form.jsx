c0ead39138d0a3d4582bb059e5bb5d7183cffd4a
lrhyde <lrhyde13@gmail.com>
2025-04-06 16:37:14 -0500

    wip

--- src/form.jsx
+++ src/form.jsx
@@ -1,8 +1,25 @@
-export const Form = ({ company }) => {
+import Rating from "@mui/material/Rating";
+// {
+//   name: "";
+//   starRatings: [];
+//   accessibility: [];
+//   inclusive: [];
+//   textReviews: [];
+// }
+export const Form = (company) => {
   return (
     <div>
       <h1>Share your experience with {company}</h1>
-      <form></form>
+      <form>
+        <Rating
+          name="simple-uncontrolled"
+          onChange={(event, newValue) => {
+            company.starRatings.push(newValue);
+          }}
+          defaultValue={0}
+        />
+        <label for="">Did you feel included at {company.name}?</label>
+      </form>
     </div>
   );
 };
